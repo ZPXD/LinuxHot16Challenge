a59eea0525631e06f114da625d5ae064af0b871f
ZPXD <zajeciaprogramowaniaxd@gmail.com>
2022-04-20 00:50:58 +0200

    Adding frontend by mg.

---
+++ app.py
@@ -0,0 +1,136 @@
+from flask import Flask, render_template
+import os
+import requests
+
+
+here = os.getcwd()
+classic_data_folder = os.path.join(here, 'data', 'wasze_zwrotki')
+
+app=Flask(__name__)
+
+
+here = os.getcwd()
+classic_data_folder = os.path.join(here, 'data', 'wasze_zwrotki')
+freestyle_data_folder = os.path.join(here, 'data', 'wasze_zwrotki_freestyle')
+
+def check_file_ANTI_MALWARE():
+	pass
+
+def get_file_data():
+	pass
+
+def get_fresh_data():
+	repo_url = 'https://github.com/ZPXD/LinuxHot16Challenge.git'
+	delete = os.path.join(classic_data_folder, 'LinuxHot16Challenge')
+	what = os.path.join(classic_data_folder, 'LinuxHot16Challenge', 'wasze_zwrotki')
+	existing_files = os.listdir(classic_data_folder)
+	os.system( 'git clone {} {}'.format(repo_url, delete) )
+	fresh_files = os.listdir(what)
+	for f in fresh_files:
+		if f not in existing_files:
+			fp1 = os.path.join(what, f)
+			fp2 = os.path.join(classic_data_folder, f)
+			os.system('cp -r {} {}'.format(fp1, fp2))
+	os.system( 'sudo rm -r {}'.format(delete))
+
+def sort_by_rank(commands_list):
+	commands_list = sorted(commands_list, key=lambda k : k['rank'])[::-1]
+	return commands_list
+
+def get_file_data_lines(fn):
+	file_path = os.path.join(classic_data_folder, fn)
+	with open(file_path, 'r') as f:
+		text_lines = f.readlines()
+	text_lines = [l.strip() for l in text_lines if '#' in l]
+	return text_lines
+
+
+def command_wiki(command):
+	url = 'https://man7.org/linux/man-pages/man1/{}.1.html'.format(command)
+	return url
+
+def command_wiki_pl(command):
+	url = 'https://linux.fandom.com/pl/wiki/{}'.format(command)
+	return url
+
+def load_classic_data():
+	
+	commands_list = []
+
+	file_names = os.listdir(classic_data_folder)
+	for fn in file_names:
+
+		if fn == 'README.md' or fn == 'TWOJEIMIE_linux_hot_16_challenge.txt':
+			continue
+
+		text_lines = get_file_data_lines(fn)
+		
+		for idx, l in enumerate(text_lines):
+			line_elements = l.split()
+
+			if len(line_elements) >= 3:	
+				ranking = line_elements[1]
+				if '.' in ranking:
+					ranking = ranking.replace('.', '')
+					if ranking.isnumeric():
+
+						command = {}
+						command['name'] = None
+						command['rank'] = None
+						command['rank_list'] = []
+						command['description'] = []
+
+						# Name:
+						name = line_elements[2]
+						other_options = line_elements[3:]
+						
+
+						name = name.strip()
+						command['name'] = name
+						if command['name'] in ['komenda']:
+							continue
+
+						# Wiki: 
+						command['wiki'] = command_wiki(name)
+						command['wiki_pl'] = command_wiki_pl(name)
+
+						# Update existing command data or add new:
+						existing_commands = [c['name'] for c in commands_list]
+						if command['name'] in existing_commands:
+							i, command = [[i, c] for [i, c] in enumerate(commands_list) if c['name'] == command['name']][0]
+							commands_list.pop(i)
+
+						# Ranking:
+						rank = int(ranking)
+						rank = 33 - rank
+						command['rank_list'].append(rank)
+						command['rank'] = sum(command['rank_list'])
+
+						# Description:
+						description = text_lines[idx+1]
+						description = description.replace('#', '')
+						description = description.strip()
+						command['description'].append(description)
+						commands_list.append(command)
+	return commands_list
+
+@app.route('/')
+def mg():
+	commands_list = load_classic_data()
+	commands_list = sort_by_rank(commands_list)
+	ranking = []
+	for i, c in enumerate(commands_list, 1):
+		c['nr'] = i
+		ranking.append(c)
+	return render_template("index.html", ranking=ranking)
+
+@app.route('/freestyle')
+def hot_16_challenge_freestyle():
+	return render_template("freestyle.html")
+
+@app.route('/dodaj_swoje_zwrotki')
+def hot_16_add_yours():
+	return render_template("freestyle.html")
+
+if __name__=="__main__":
+	app.run()
---
+++ data/wasze_zwrotki/README.md
@@ -0,0 +1,17 @@
+# Zasady #LinuxHot16Challenge
+
+#### Tu wrzucamy wasze pomysły.
+
+1. Otwórz plik
+2. Wybierz 16 najważniejszych komend i 16 kolejnych, razem 32.
+3. Spisz całość trzymając się formatu w  TWOJEIMIE_linux_hot_16_challenge.txt, czyli: 1 linia z komendą i zaraz po niej 1 max linia opisu. Ale napisz kompletnie po swojemu jak chodzi o treść:
+- to może byc rap
+- może być 13-sto zgłoskowcem
+- może być super techniczny osób a może być bardziej kreatywnie 
+- a może być totalnie po Twojemu
+4. Jak Twój poemat, rap, instrukcja czy innego rodzaju dzieło będzie gotowe aby ujżeć światło dzienne:
+5. Pamiętaj o zmianie nazwy na TWOJEIMIE_linux_hot_16_challenge.txt <- zamiast TWOJEIMIE napisz swoje imie w sposob który będzie w miare unikalny(w sensie, jak masz na imie Paweł to moze sie zdarzyc pare razy ze ktos napisze pawel_linux_hot_16_challenge.txt - wiec lepiej napisz tu_pawel_elo_sprzedam_opla_xd_linux_hot_16_challenge.txt ;) )
+6. Wrzuć plik tutaj
+7. Wrzuć na fb / twitterze / gdzie chcesz posta z linkiem do Twoih 16 gorących komend Linuxa z hasztagiem #LinuxHot16Challenge 
+8. Sprawdzaj rezultaty na www.xd.xd [strona będzie w krótce]
+9. Dzięki wygenerowanej liście ustalimy wspólnie propozycje rankingu najważniejszych komend linuxa do poznania na początku.
